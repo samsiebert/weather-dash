5910a41d460eaecfe59e5322b8f107a79722aaf8
samsiebert <sammsiebert@gmail.com>
2022-06-12 12:03:15 -0500

    connect geocode and weather api functions, begin forming print to page functions

--- assets/js/script.js
+++ assets/js/script.js
@@ -1,30 +1,54 @@
 var cityFormEl = document.querySelector("#search-form");
 var cityInputEl = document.querySelector("#city-search");
 var date = dayjs().format("MM/DD/YYYY");
-var city = "";
+var cityName = "";
 var lat = "";
-var long = "";
-var apiUrl = "https://api.openweathermap.org/data/2.5/onecall?lat=" + lat + "&lon=" + long + "&exclude=current,minutely,hourly,alerts&appid=a74391bcfbdf1e9827d65a7e2e76f024";
+var lon = "";
+// var apiUrl = "https://api.openweathermap.org/data/2.5/onecall?lat=" + lat + "&lon=" + lon + "&exclude=current,minutely,hourly,alerts&appid=a74391bcfbdf1e9827d65a7e2e76f024";
 var todaysTemp = "";
 var todaysWind = "";
 var todaysHumidity = "";
-var todaysUv = "";
+var todaysUvi = "";
 
 var cityFormHandler = function() {
     event.preventDefault();
-    city = cityInputEl.value.trim();
-    console.log(city);
-    console.log(date);
+    cityName = cityInputEl.value.trim();
+    var geocodeUrl = "http://api.openweathermap.org/geo/1.0/direct?q=" + cityName + "&limit=1&appid=a74391bcfbdf1e9827d65a7e2e76f024"
+    fetch(geocodeUrl).then(function(response) {
+        response.json().then(function(data) {
+            console.log(data);
+            lat = data[0].lat;
+            lon= data[0].lon;
+            getWeather();
+        });
+    });
 };
 
 
 
 var getWeather = function() {
-fetch(apiUrl).then(function(response) {
-    response.json().then(function(data) {
+    var apiUrl = "https://api.openweathermap.org/data/2.5/onecall?lat=" + lat + "&lon=" + lon + "&exclude=minutely,hourly,alerts&units=imperial&appid=a74391bcfbdf1e9827d65a7e2e76f024";
+    fetch(apiUrl).then(function(response) {
+        response.json().then(function(data) {
         console.log(data);
+        var weather = data;
+
+        todaysTemp = weather.current.temp;
+        todaysWind = weather.current.wind_speed;
+        todaysHumidity = weather.current.humidity;
+        todaysUvi = weather.current.uvi;
+
+        printToday(todaysTemp, todaysWind, todaysHumidity, todaysUvi);
+
+        });
     });
-});
+};
+
+var printToday = function(temp, wind, humidity, uvi) {
+    console.log("The current temp is: " + temp + "F");
+    console.log("The current wind speed is: " + wind + "mph");
+    console.log("The current humidity is: " + humidity + "%");
+    console.log("The current UVI is: " + uvi + "uvi");
 };
 
 cityFormEl.addEventListener("submit", cityFormHandler);
